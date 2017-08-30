50d73ccb2713c04d3731d034c29c6189699ddc16
palash16 <konnectpalash@gmail.com>
2017-08-30 22:26:23 +0530

    Spec 5: done fg and bg left

--- shell.c
+++ shell.c
@@ -140,8 +140,7 @@ int cd(char **args) {
 	return 1;
 }	
 
-int pinfo(char **args,char *path)
-{
+int pinfo(char **args,char *path) {
 	char ProcessPath[1000];
 	strcpy(ProcessPath,"/proc/");
 	if(args[1]==NULL)
@@ -158,10 +157,10 @@ int pinfo(char **args,char *path)
 	//to print pid and process status
 	int pid;
 	char status,name[50];
-	FILE *stat=fopen(finalpath,"r");
-	fscanf(stat,"%d %s %c",&pid,name,&status);
+	FILE *stat1=fopen(finalpath,"r");
+	fscanf(stat1,"%d %s %c",&pid,name,&status);
 	fprintf(stdout," pid: %d\n Process Status: %c\n",pid,status);
-	fclose(stat);
+	fclose(stat1);
 
 
 	//to print memory
@@ -181,21 +180,18 @@ int pinfo(char **args,char *path)
 	readlink(finalpath,execpath,sizeof(execpath));
 
 	int i=0,pathL=strlen(path);
-	while(i<pathL)
-	{
+	while(i<pathL) {
 		if(path[i]!=execpath[i])
 			break;
 		i++;
 	}
 
-	if(i==pathL)
-	{
+	if(i==pathL) {
 		relativepath[0]='~';
 		relativepath[1]='\0';
 		strcat(relativepath,(const char *)&execpath[pathL]);
 	}
-	else
-	{
+	else {
 		strcpy(relativepath,execpath);
 		relativepath[strlen(execpath)]='\0';
 	}
@@ -205,6 +201,7 @@ int pinfo(char **args,char *path)
 
 	fprintf(stdout,"Executable Path: %s\n",relativepath);
 	return 1;
+}
 
 int ls(char **args) {
 	struct dirent **dent;
